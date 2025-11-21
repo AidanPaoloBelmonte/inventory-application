df3d6afac09d495d537a13b1e1dfb68abde81b6b
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-11-21 16:13:19 +0800

    feat: Allowed pool.js to connect with a database url

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,8 @@ const { Pool } = require("pg");
 
 require("dotenv").config();
 
-module.exports = new Pool({
-  host: process.env.SQL_ADDRESS,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
+const connectionProps = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL }
+  : `postgresql://${process.env.USER}:${process.env.PASSWORD}@localhost:5432/${process.env.DATABASE}`;
+
+module.exports = new Pool(connectionProps);
